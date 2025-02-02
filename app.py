d8873e893ac855e09fac7b24049ef1ee56d7fb35
mohdUwaish59 <mohd.uwaish.e21@nsut.ac.in>
2025-02-02 19:02:24 +0100

    fix: Adding logo

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ if 'generated_answer' not in st.session_state:
 # Display Title with Image
 col1, col2 = st.columns([1, 10])
 with col1:
-    st.image("assets/dna.png", width=100)  # ✅ Correctly loads image
+    st.image("assets/dna.png", width=100)  
 with col2:
     st.title("BioBrain")
     st.subheader("Your biology mentor for Neet Exam")
