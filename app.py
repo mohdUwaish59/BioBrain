76bc4136a042c144374914e2092cec1e358ee83c
mohdUwaish59 <mohd.uwaish.e21@nsut.ac.in>
2025-02-02 19:03:54 +0100

    fix: Adding logo

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ if st.button("Submit Question", key="submit_query"):
 
 # Display the generated answer
 if st.session_state.generated_answer:
-    st.subheader("📝 AI Response")
+    st.subheader("📝 BioBrain's Response:")
     with st.container():
         st.write(st.session_state.generated_answer)
 
